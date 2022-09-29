7f2fd7008eeb9af4431665240c77017872593cc6
Veervikram singh <veervikramsingh132@gmail.com>
2022-09-29 20:29:05 +0530

    Deployed SC

---
+++ projects/LearnWeb3io/Sophomore Track/NFT-DAO/hardhat/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x9608d0245253d7d9b2b49609D8e4AAa0eF533cbC";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
