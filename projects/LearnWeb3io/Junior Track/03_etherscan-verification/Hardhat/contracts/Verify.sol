2a70cbbf556a175576989ba505370da134d1a113
Veervikram singh <veervikramsingh132@gmail.com>
2022-10-04 15:36:53 +0530

    test

--- projects/LearnWeb3io/Junior Track/03_etherscan-verification/Hardhat/contracts/Verify.sol
+++ projects/LearnWeb3io/Junior Track/03_etherscan-verification/Hardhat/contracts/Verify.sol
@@ -1,5 +1,9 @@
 //SPDX-License-Identifier: Unlicensed
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.7;
+
+// deployed :
+//  on goerli: "0xD23e54d7F00bB607e5EbcCBdf2e7DfA2ca437C48"
+//  on polygon:"0x8E9223FdA24A67D5675C99f16FF15dBb38d79827"
 
 contract Verify {
     string private greeting;
