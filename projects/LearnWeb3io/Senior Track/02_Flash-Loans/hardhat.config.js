f37637af7c68dbe20dc267add846375c4f90d03c
Veervikram singh <veervikramsingh132@gmail.com>
2022-10-09 18:54:39 +0530

    Aave

--- projects/LearnWeb3io/Senior Track/02_Flash-Loans/hardhat.config.js
+++ projects/LearnWeb3io/Senior Track/02_Flash-Loans/hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const QUICKNODE_RPC_URL = process.env.QUICKNODE_RPC_URL;
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.10",
+  networks: {
+    hardhat: {
+      forking: {
+        url: QUICKNODE_RPC_URL,
+      },
+    },
+  },
 };
