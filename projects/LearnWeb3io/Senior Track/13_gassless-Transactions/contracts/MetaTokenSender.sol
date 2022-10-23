fe1da4e2b8a3864e3e7937cdfad0484d5b8054e4
Veervikram singh <veervikramsingh132@gmail.com>
2022-10-23 21:22:57 +0530

    completed freshmen

--- projects/LearnWeb3io/Senior Track/13_gassless-Transactions/contracts/MetaTokenSender.sol
+++ projects/LearnWeb3io/Senior Track/13_gassless-Transactions/contracts/MetaTokenSender.sol
@@ -15,20 +15,37 @@ contract RandomToken is ERC20 {
 contract TokenSender {
     using ECDSA for bytes32;
 
+    // New mapping
+    mapping(bytes32 => bool) executed;
+
+    // Add the nonce parameter here
     function transfer(
         address sender,
         uint amount,
         address recipient,
         address tokenContract,
+        uint nonce,
         bytes memory signature
     ) public {
-        bytes32 messageHash = getHash(sender, amount, recipient, tokenContract);
+        // Pass ahead the nonce
+        bytes32 messageHash = getHash(
+            sender,
+            amount,
+            recipient,
+            tokenContract,
+            nonce
+        );
         bytes32 signedMessageHash = messageHash.toEthSignedMessageHash();
+
+        // Require that this signature hasn't already been executed
+        require(!executed[signedMessageHash], "Already executed!");
 
         address signer = signedMessageHash.recover(signature);
 
         require(signer == sender, "Signature does not come from sender");
 
+        // Mark this signature as having been executed now
+        executed[signedMessageHash] = true;
         bool sent = ERC20(tokenContract).transferFrom(
             sender,
             recipient,
@@ -37,15 +54,23 @@ contract TokenSender {
         require(sent, "Transfer failed");
     }
 
+    // Add the nonce parameter here
     function getHash(
         address sender,
         uint amount,
         address recipient,
-        address tokenContract
+        address tokenContract,
+        uint nonce
     ) public pure returns (bytes32) {
         return
             keccak256(
-                abi.encodePacked(sender, amount, recipient, tokenContract)
+                abi.encodePacked(
+                    sender,
+                    amount,
+                    recipient,
+                    tokenContract,
+                    nonce
+                )
             );
     }
 }
